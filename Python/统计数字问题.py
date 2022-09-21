ea044e9975d77e986759de35fd982cd4e3cd16ca
robotlwy <82713667+robotlwy@users.noreply.github.com>
2022-09-21 10:44:45 +0800

    Update 统计数字问题.py

--- Python/统计数字问题.py
+++ Python/统计数字问题.py
@@ -19,20 +19,23 @@ def count(page):
         numberCountList[i] += 10 ** (length - 1)
     #统计除other即23上0~9出现的次数
     other = page % 10 ** (length - 1)
+    #若page形式为x0000……（x为1~9中的一个数，n个0，n为自然数），则把最高位+1和加上n个0就统计完成
     if (other == 0):
         numberCountList[maxBit] += 1
         numberCountList[0] += length - 1
         return
     otherLength =int(math.log10(other)) + 1
+    #当other != length - 1时，说明page中有0
     if (other != length - 1):
         numberCountList[0] += (length - 1 - otherLength) * (other + 1)
     numberCountList[maxBit] += other + 1
     #统计other即23上出现的次数
-    return count(other)#留下低位计算
+    return count(other)#留下低位继续计算
 
 
 length =int(math.log10(page)) + 1
 count(page)
+#把多加的0减去
 numberCountList[0] -= int((10 ** length - 1)/9)
 with open('output.txt', 'w') as f:
     for i in range(10):
